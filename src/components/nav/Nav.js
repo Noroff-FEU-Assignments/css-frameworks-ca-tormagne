dbd7ad7dcab7950910a2b6c68e2d4a0f302a814a
Tor Magne <tormagne-brekken@outlook.com>
2021-10-08 08:32:11 +0200

    commit

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -20,7 +20,7 @@ function nav() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
-                <Nav.Link className="nav__active px-4" as={Link} exact to={'/'}>
+                <Nav.Link className=" px-4" as={Link} exact to={'/'}>
                   Home
                 </Nav.Link>
                 <Nav.Link as={Link} to={'/news'}>
