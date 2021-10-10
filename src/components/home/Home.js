70c168d104b8fdcd06c0b47149709feb45602ccf
Tor Magne <tormagne-brekken@outlook.com>
2021-10-10 13:29:17 +0200

    added carousel and images to carousel

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import Cimg1 from '../../images/carousel/carousel-1.jpg';
+import Cimg2 from '../../images/carousel/carousel-2.jpg';
+import Cimg3 from '../../images/carousel/carousel-3.jpg';
 
 function Home() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <>
+      <div className="">
+        <Carousel controls={false} className="carousel-header">
+          <Carousel.Item>
+            <img className="d-block w-100" src={Cimg1} alt="First slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={Cimg2} alt="Second slide" />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img className="d-block w-100" src={Cimg3} alt="Third slide" />
+          </Carousel.Item>
+        </Carousel>
+      </div>
+    </>
   );
 }
 
