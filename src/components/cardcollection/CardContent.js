d1fab067a614af65fd57e2d73d7b2b172fe98c99
Tor Magne <tormagne-brekken@outlook.com>
2021-10-11 18:02:55 +0200

    added card title props

--- src/components/cardcollection/CardContent.js
+++ src/components/cardcollection/CardContent.js
@@ -11,6 +11,7 @@ import CardImg7 from '../../images/news/news-7.jpg';
 import CardImg8 from '../../images/news/news-8.jpg';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import CardTitle from '../cardtitle/CardTitle';
 
 function CardContent() {
   return (
@@ -20,7 +21,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg1} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -30,7 +31,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg2} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -40,7 +41,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg3} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -50,7 +51,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg4} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -62,7 +63,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg5} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -72,7 +73,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg6} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -82,7 +83,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg7} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
@@ -92,7 +93,7 @@ function CardContent() {
           <Card style={{ width: '16.5rem' }}>
             <Card.Img variant="top" src={CardImg8} />
             <Card.Body>
-              <Card.Title>Nunc porttitor vel</Card.Title>
+              <CardTitle content="Nunc porttitor vel" />
               <Card.Text>Nunc malesuada eget est fringilla dapibus.</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
