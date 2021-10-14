b852eac725862a0c8f14c050819c76acd62197fa
Tor Magne <tormagne-brekken@outlook.com>
2021-10-14 10:11:55 +0200

    remove all files

--- src/components/cardtitle/CardTitle.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-
-function CardTitle(props) {
-  return <Card.Title>{props.content}</Card.Title>;
-}
-
-export default CardTitle;
